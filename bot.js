36064a512e199825eb0b05d64cd4c988ef18a5a1
AustinCreates154 <41809488+AustinCreates154@users.noreply.github.com>
2018-08-23 08:20:18 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -20,23 +20,6 @@ bot.on("message", message => {
   }
 });
 
-bot.on("message", message => {
-  const args = message.content.split(" ").slice(1);
 
-  if (message.content === "vbd.eval") {
-    if(message.author.id !== config.ownerID) return;
-    try {
-      const code = args.join(" ");
-      let evaled = eval(code);
-
-      if (typeof evaled !== "string")
-        evaled = require("util").inspect(evaled);
-
-      message.channel.send(clean(evaled), {code:"xl"});
-    } catch (err) {
-      message.channel.send(`\`ERROR\` \`\`\`xl\n${clean(err)}\n\`\`\``);
-    }
-  }
-});
 
 bot.login(process.env.BTOKEN)
