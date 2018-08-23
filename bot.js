e8378025120e4b4eb8f8ac625d23bf60545ab32d
AustinCreates154 <41809488+AustinCreates154@users.noreply.github.com>
2018-08-23 07:41:59 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,4 +3,9 @@ const bot = new Discord.Client();
 const config = require(`./config.json`)
 //Basic Constants
 
+bot.on("ready", () => {
+  console.log(`I am logged in as ${bot.user.tag}`)
+  bot.user.setActivity('Hello there!')
+})
+
 bot.login(process.evl.BTOKEN)
